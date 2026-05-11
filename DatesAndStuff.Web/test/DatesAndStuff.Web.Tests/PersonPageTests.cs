4c3e44275ed97d3b0deea177a0237187c6ecac31
Orsolya Toth <toth.orsolya2004@gmail.com>
2026-05-11 14:48:02 +0300

    lab4-3

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -157,6 +157,37 @@ public class PersonPageTests
 
         fieldValidationError.Text.Should().NotBeNullOrWhiteSpace();
     }
+
+    [Test]
+    public void Person_SalaryIncreaseMinusTen_ShouldUpdateSalary()
+    {
+        // Arrange
+        driver.Navigate().GoToUrl(BaseURL);
+        driver.FindElement(By.XPath("//*[@data-test='PersonPageNavigation']")).Click();
+
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        var input = wait.Until(ExpectedConditions.ElementExists(
+            By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
+
+        input.Clear();
+        input.SendKeys("-10");
+
+        // Act
+        var submitButton = wait.Until(ExpectedConditions.ElementExists(
+            By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']")));
+
+        submitButton.Click();
+
+        // Assert
+        var salaryLabel = wait.Until(ExpectedConditions.ElementExists(
+            By.XPath("//*[@data-test='DisplayedSalary']")));
+
+        var salaryAfterSubmission = double.Parse(salaryLabel.Text);
+        salaryAfterSubmission.Should().BeApproximately(4500, 0.001);
+    }
+
+
     private bool IsElementPresent(By by)
     {
         try
